77137bc8b68b21d5fae7484f259fc64b843f71b6
agates4 <agate97@gmail.com>
2017-08-25 16:02:11 -0400

    more updates to the dashboard

---
+++ static/sidebar.js
@@ -0,0 +1,31 @@
+$("#chartsRow").css('background','#47525D')
+var selectedRow = "#chartsRow";
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+$("#charts").click(function(e) {
+    e.preventDefault();
+    $(selectedRow).css('background','')
+    $(selectedRow + "Body").hide();
+    $("#chartsRowBody").show();
+    $("#chartsRow").css('background','#47525D')
+    selectedRow = "#chartsRow";
+});
+$("#heatmap").click(function(e) {
+    e.preventDefault();
+    $(selectedRow).css('background','')
+    $(selectedRow + "Body").hide();
+    $("#heatmapRowBody").show();
+    $("#heatmapRow").css('background','#47525D')
+    selectedRow = "#heatmapRow";
+});
+$("#settings").click(function(e) {
+    e.preventDefault();
+    $(selectedRow).css('background','')
+    $(selectedRow + "Body").hide();
+    $("#settingsRowBody").show();
+    $("#settingsRow").css('background','#47525D')
+    selectedRow = "#settingsRow";
+});
